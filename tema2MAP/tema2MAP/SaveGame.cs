667485948aa3008a31e007629ba1e2c8bca174a5
Ioanaa2 <ioanapana46@yahoo.com>
2026-04-20 23:54:02 +0300

    add timer

---
+++ tema2MAP/tema2MAP/SaveGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tema2MAP
+{
+    public class SavedGame
+    {
+        public string PlayerName { get; set; }
+        public string Category { get; set; }
+
+        public int Level { get; set; }
+        public int Streak { get; set; }
+        public int WrongGuesses { get; set; }
+        public int TimeLeft { get; set; }
+
+        public string Word { get; set; }
+        public string DisplayWord { get; set; }
+
+        public List<char> UsedLetters { get; set; }
+    }
+}
